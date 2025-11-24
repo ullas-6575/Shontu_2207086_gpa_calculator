e5e5c04cc4fe891c9ccf0b878353129856648077
ullas biswas shontu <ullasbsantu@gmail.com>
2025-11-25 00:56:03 +0600

    table created

---
+++ src/main/java/com/example/gpacalc/database.java
@@ -0,0 +1,83 @@
+package com.example.gpacalc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class database {
+
+
+    private static final String URL = "jdbc:sqlite:gpacalc.db";
+
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL);
+            //System.out.println("Connected to SQLite.");
+        } catch (SQLException e) {
+            System.out.println("Connection failed!");
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void initialize() {
+        String sql = """
+                CREATE TABLE IF NOT EXISTS Courses (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    course_name TEXT NOT NULL,
+                    credit INTEGER NOT NULL,
+                    grade TEXT NOT NULL
+                );
+                """;
+
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement()) {
+
+            stmt.execute(sql);
+//            System.out.println("Database initialized.");
+
+        } catch (SQLException e) {
+            System.out.println("Initialization failed!");
+            e.printStackTrace();
+        }
+    }
+    public void insertCourse(String name, int credit, String grade) {
+        String sql = "INSERT INTO Courses(course_name, credit, grade) VALUES(?, ?, ?)";
+
+        try (Connection conn = connect();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            pstmt.setString(1, name);
+            pstmt.setInt(2, credit);
+            pstmt.setString(3, grade);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            System.err.println("Insert failed for course " + name + ". Error: " + e.getMessage());
+        }
+    }
+    public void getAllCourses() {
+        String sql = "SELECT id, course_name, credit, grade FROM Courses";
+
+        try (Connection conn = connect();
+             Statement stmt  = conn.createStatement();
+             ResultSet rs    = stmt.executeQuery(sql)){
+
+            while (rs.next()) {
+
+                int id = rs.getInt("id");
+                String name = rs.getString("course_name");
+                int credit = rs.getInt("credit");
+                String grade = rs.getString("grade");
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -10,6 +10,7 @@ module com.example.gpacalc {
     requires org.kordamp.bootstrapfx.core;
     requires eu.hansolo.tilesfx;
     requires com.almasb.fxgl.all;
+    requires java.sql;
 
     opens com.example.gpacalc to javafx.fxml;
     exports com.example.gpacalc;
